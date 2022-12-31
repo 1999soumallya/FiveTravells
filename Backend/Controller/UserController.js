11da8850dd189c66a0c00e4e898b0a920ab648dc
1999soumallya <1999soumallya@gmail.com>
2022-12-31 17:45:30 +0530

    Get Flight Data

--- Backend/Controller/UserController.js
+++ Backend/Controller/UserController.js
@@ -3,7 +3,6 @@ const { ConnectMysql } = require('../Config/Connection')
 const FileUploadModel = require('../Models/FileUploadModel')
 const FormatMongoData = require('../utils/MongoFormatData')
 const Constants = require('../Constants/Constants')
-const isodate = require("isodate");
 
 const connection = ConnectMysql()
 
@@ -16,30 +15,21 @@ module.exports.AirportDetails = asyncHandler(async (req, res) => {
 
 module.exports.GetFlightDetails = asyncHandler(async (req, res) => {
     const { Destination, Origin, Depture_Date } = req.body
-    const FilterFlightSector = await FileUploadModel.findOne({ SECTOR: Destination.toUpperCase() })
-    if (FilterFlightSector) {
-        const FilerFlightForm = await FileUploadModel.findOne({ FORM: Origin.toUpperCase() })
-        if (FilerFlightForm) {
-            const FilerFlightDepture_Date = await FileUploadModel.findOne({ DEPARTURE_DATE: isodate(Depture_Date) })
-            if (FilerFlightDepture_Date) {
-                const Filter = await FileUploadModel.find({ SECTOR: Destination.toUpperCase(), FORM: Origin.toUpperCase(), DEPARTURE_DATE: isodate(Depture_Date) })
-                if (Filter) {
-                    res.status(200).send(FormatMongoData(Filter))
-                }
-            } else {
-                res.status(404).send(Constants.CommonQueryMessage.NO_FLIGHT_DETAILS(Depture_Date))
-            }
+    await connection.query(`SELECT DISTINCT * FROM FlightDetails WHERE DEPARTURE_DATE = '${Depture_Date}' AND FORM = '${Origin}' AND SECTOR= '${Destination}'`, (err, result) => {
+        if (err) console.log(err);
+        if (result.length > 0) {
+            res.status(200).json(result);
         } else {
-            res.status(404).send(Constants.CommonQueryMessage.NO_FLIGHT_DETAILS(Origin))
+            res.status(404).send(Constants.CommonQueryMessage.NO_FLIGHT_DETAILS('Week'))
         }
-    } else {
-        res.status(404).send(Constants.CommonQueryMessage.NO_FLIGHT_DETAILS(Destination))
-    }
+    })
 })
 
 module.exports.GetWeeklyFlightDetails = asyncHandler(async (req, res) => {
     let date = new Date();
     date.setDate(date.getDate() + 7);
+    console.log(new Date().toISOString());
+    return
     const FlightDeta = await FileUploadModel.find({ DEPARTURE_DATE: { $gte: new Date(), $lte: date } })
     if (FlightDeta.length > 0) {
         res.status(200).send(FormatMongoData(FlightDeta))
