334a2cc85b2438b9c009b3a334a88754ca0c4c86
1999soumallya <1999soumallya@gmail.com>
2023-01-09 12:19:45 +0530

    Add Admin Pre Flight Booking Section

--- Frontend/src/Components/Admin/AdminFileUpload.jsx
+++ Frontend/src/Components/Admin/AdminFileUpload.jsx
@@ -107,7 +107,7 @@ export default function AdminFileUpload() {
             </div>
             <div className='mt-3'>
                 {
-                    loading ? <DnaLoader /> : error ? <ErrorAlert variant={"danger"} children={error} /> : (allFlight.length === 0) ? <ErrorAlert variant={"success"} children={"No Air line Deta found"} /> : (allFlight.length > 0) && (
+                    loading ? <DnaLoader /> : error ? <ErrorAlert variant={"danger"} children={error} /> : (JSON.stringify(allFlight) === '[]') ? <ErrorAlert variant={"success"} children={"No Air line Deta found"} /> : (
                         <DataTable keyField='id' title="Air Line Details" data={allFlight} columns={columns} pagination />
                     )
                 }
